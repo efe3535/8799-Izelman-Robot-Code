954499df477bb3e0780acee6411f822509e6b7c4
A. Efe AKYAZI <a.efe.akyazi@gmail.com>
2024-02-26 19:51:29 +0300

    updates

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,11 +6,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.drive.Drive;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,12 +35,26 @@ public class Robot extends TimedRobot {
 
   private final CommandXboxController m_controller = new CommandXboxController(0);
 
-  private final WPI_VictorSPX m_rearLeft = new WPI_VictorSPX(0);
-  private final WPI_VictorSPX m_rearRight = new WPI_VictorSPX(1);
-  private final WPI_VictorSPX m_frontLeft = new WPI_VictorSPX(2);
-  private final WPI_VictorSPX m_frontRight = new WPI_VictorSPX(3);
+  private final CANSparkMax m_rearLeft = new CANSparkMax(3, MotorType.kBrushless);
+  private final CANSparkMax m_rearRight = new CANSparkMax(2, MotorType.kBrushless);
 
-  public final Drive m_drive = new Drive(m_rearLeft, m_rearRight, m_frontLeft, m_frontRight, true);
+  private final CANSparkMax m_frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+  private final CANSparkMax m_frontRight = new CANSparkMax(4, MotorType.kBrushless);
+
+  private final CANcoder m_leftEncoder = new CANcoder(6);
+  private final CANcoder m_rightEncoder = new CANcoder(5);
+
+  // private DifferentialDrive drive = new DifferentialDrive(, );
+
+  private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS1);
+  private Pose2d m_pose = new Pose2d(5.0, 13.5, new Rotation2d());
+
+  private final double kDriveTick2Feet = 1.0 / 4096 * 6 * Math.PI / 12;
+  private final double kArmTick2Deg = 360.0 / 512 * 26 / 42 * 18 / 60 * 18 / 84;
+
+  String trajectoryJSON = "output/Unnamed.wpilib.json";
+  Trajectory trajectory = new Trajectory();
+  public final Drive m_drive = new Drive(m_rearLeft, m_rearRight, m_frontLeft, m_frontRight, true); // normalde true
 
   private final Timer m_timer = new Timer();
 
@@ -43,6 +67,10 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_drive.init();
     DataLogManager.start();
+    m_gyro.calibrate();
+    m_leftEncoder.setPosition(0);
+    m_rightEncoder.setPosition(0);
+
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
@@ -66,25 +94,36 @@ public class Robot extends TimedRobot {
   public void teleopInit() {
     m_timer.reset();
     m_timer.start();
-    m_controller.rightTrigger().onTrue(m_drive.ToggleBrake());
   }
 
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_drive.drive(m_controller.getLeftY(), m_controller.getLeftX());
+    m_drive.drive(m_controller.getLeftY(), m_controller.getRightX());
 
   }
 
   /** This function is called once each time the robot enters test mode. */
   @Override
   public void testInit() {
-
+    m_timer.reset();
+    m_timer.start();
+    // m_drive.drive(0, 0.25);
   }
 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
 
+    /*
+     * m_pose = m_odometry.update(m_gyro.getRotation2d(),
+     * m_leftEncoder.getPosition().getValueAsDouble(),
+     * m_rightEncoder.getPosition().getValueAsDouble());
+     * 
+     * DataLogManager.log(m_pose.toString());
+     */
+    DataLogManager.log("angle" + m_gyro.getAngle());
+    DataLogManager.log("left encoder: " + m_leftEncoder.getPosition().getValue());
+    DataLogManager.log("right encoder: " + m_rightEncoder.getPosition().getValue());
   }
 }
--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -1,20 +1,20 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.InvertType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.ReplanningConfig;
+import com.revrobotics.CANSparkMax;
 
 public class Drive extends SubsystemBase {
     public DifferentialDrive m_drive;
-    public WPI_VictorSPX rearLeft, rearRight, frontLeft, frontRight;
+    public CANSparkMax rearLeft, rearRight, frontLeft, frontRight;
     public boolean isReversed;
-    public NeutralMode currentMode = NeutralMode.Coast;
 
-    public Drive(WPI_VictorSPX rearLeft, WPI_VictorSPX rearRight, WPI_VictorSPX frontLeft, WPI_VictorSPX frontRight,
+    public Drive(CANSparkMax rearLeft, CANSparkMax rearRight, CANSparkMax frontLeft, CANSparkMax frontRight,
             boolean reverse) {
         this.rearLeft = rearLeft;
         this.rearRight = rearRight;
@@ -23,42 +23,25 @@ public class Drive extends SubsystemBase {
         this.isReversed = reverse;
     }
 
-    public Command ToggleBrake() {
-        return this.runOnce(() -> this.toggleBrake());
-    }
-
-    private void toggleBrake() {
-        rearLeft.setNeutralMode(currentMode == NeutralMode.Coast ? NeutralMode.Brake : NeutralMode.Coast);
-        rearRight.setNeutralMode(currentMode == NeutralMode.Coast ? NeutralMode.Brake : NeutralMode.Coast);
-        frontLeft.setNeutralMode(currentMode == NeutralMode.Coast ? NeutralMode.Brake : NeutralMode.Coast);
-        frontRight.setNeutralMode(currentMode == NeutralMode.Coast ? NeutralMode.Brake : NeutralMode.Coast);
+    public void test() {
+        frontLeft.set(0.25);
+        rearLeft.set(0.25);
     }
 
     public void init() {
-        rearLeft.configFactoryDefault();
-        frontLeft.configFactoryDefault();
-        rearRight.configFactoryDefault();
-        frontRight.configFactoryDefault();
-
-        rearLeft.setNeutralMode(currentMode);
-        frontLeft.setNeutralMode(currentMode);
-        rearRight.setNeutralMode(currentMode);
-        frontRight.setNeutralMode(currentMode);
-
         rearLeft.follow(frontLeft);
         rearRight.follow(frontRight);
 
         frontLeft.setInverted(isReversed);
+        rearLeft.setInverted(isReversed);
         frontRight.setInverted(!isReversed);
-
-        rearLeft.setInverted(InvertType.FollowMaster);
-        rearRight.setInverted(InvertType.FollowMaster);
+        rearRight.setInverted(!isReversed);
 
         this.m_drive = new DifferentialDrive(frontLeft, frontRight);
     }
 
     public void drive(double leftMotor, double rightMotor) {
-        this.m_drive.arcadeDrive(leftMotor, rightMotor);
+        this.m_drive.arcadeDrive(leftMotor * 1, rightMotor * 1);
     }
 
     public DifferentialDrive getDifferentialDrive() {
